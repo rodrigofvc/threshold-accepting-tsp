9561ef22b04abc5acd58275be4e3056449c972ab
RodrigoVelazquez99 <rodrigo_velazquez99@ciencias.unam.mx>
2021-07-09 23:47:14 -0500

    new design for city

--- src/heuristics/city.rs
+++ src/heuristics/city.rs
@@ -3,45 +3,47 @@ use core::cmp::Ordering;
 #[derive(Clone, Debug)]
 pub struct City {
     pub id: u32,
-    pub x_axis: f32,
-    pub y_axis: f32
+    pub name: String,
+    pub country: String,
+    pub population: u32,
+    pub latitude: f32,
+    pub longitude: f32,
 }
 
 impl City {
     /**
     * Create a new City.
-    * name: name of city.
-    * x_axis: position in X axis.
-    * y_axis: position in Y axis.
     */
-    pub fn new(id: u32, x_axis: f32, y_axis: f32) -> City {
-        City {id, x_axis, y_axis}
+    pub fn new(id: u32, name: String, country: String, population: u32, latitude: f32, longitude: f32) -> City {
+        City {id, name, country, population, latitude,longitude}
     }
 
 
     /**
-    * Get euclidean distance with other city.
+    * Get distance.
     */
     pub fn get_distance(&self, other: City) -> f32 {
-        let x_subs = other.x_axis - self.x_axis;
-        let y_subs = other.y_axis - self.y_axis;
-        let x_subs = x_subs.powf(2.0);
-        let y_subs = y_subs.powf(2.0);
-        let adds = x_subs + y_subs;
-        return adds.sqrt();
+        let mut distance = 0.0;
+        let r = 6373000.0;
+        let difference_latitude = (self.latitude.to_radians() - other.latitude.to_radians())/2.0;
+        let product_lattitude = self.latitude.to_radians().cos() * other.latitude.to_radians().cos();
+        let difference_longitude = (self.longitude.to_radians() - other.longitude.to_radians())/2.0;
+        let a = difference_latitude.sin().powf(2.0) + product_lattitude * difference_longitude.sin().powf(2.0);
+        let c = 2.0 * a.sqrt().atan2((1.0-a).sqrt());
+        return r*c;
     }
 
     /**
-    * Return x coordenade and y coordenade.
+    * Return latitude and longitude.
     * # X Y
     * 1.23 3.45
     */
     pub fn to_string(&self) -> String {
         let mut content = String::new();
         content.push(' ');
-        content.push_str(&self.x_axis.to_string());
+        content.push_str(&self.latitude.to_string());
         content.push(' ');
-        content.push_str(&self.y_axis.to_string());
+        content.push_str(&self.longitude.to_string());
         content.push('\n');
         content
     }
@@ -51,27 +53,22 @@ impl City {
 
 impl PartialEq for City {
     fn eq(&self, other : &Self) -> bool {
-        self.x_axis == other.x_axis &&
-        self.y_axis == other.y_axis
+        self.id == other.id
     }
 }
 
 impl PartialOrd for City {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.x_axis.partial_cmp(&other.x_axis)
+        self.id.partial_cmp(&other.id)
     }
 }
 
-impl Eq for City{}
+impl Eq for City {}
 
-impl Ord for City{
+impl Ord for City {
     fn cmp(&self, other: &Self) -> Ordering {
-        if self.x_axis == other.x_axis &&
-           self.y_axis == other.y_axis {
+        if self.id == other.id {
             return Ordering::Equal;
-        } else if self.x_axis >= other.x_axis &&
-                  self.y_axis >= other.y_axis {
-            return Ordering::Greater;
         }
         return Ordering::Less;
     }
@@ -81,10 +78,13 @@ impl Ord for City{
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_euclidean_distance(){
-        let a = crate::heuristics::city::City::new(1, 34.4, 54.6);
-        let b = crate::heuristics::city::City::new(2, 12.3, 18.6);
-        let distance = a.get_distance(b);
-        assert!((42.0..43.0).contains(&distance));
+    fn test_distance(){
+        let a = crate::heuristics::city::City::new(1, String::from("Tokyo"), String::from("Japan"), 31480498,35.685000000000002273,139.75100000000000477);
+        let b = crate::heuristics::city::City::new(7, String::from("Manila"), String::from("Philippines"), 10443877,14.604200000000000514,120.98199999999999931);
+        let c = crate::heuristics::city::City::new(9, String::from("Seoul"), String::from("South Korea"), 10323448,37.598500000000001364,126.97799999999999443);
+        let mut distance = a.get_distance(b);
+        assert!((2000000.0..3100000.0).contains(&distance));
+        distance = a.get_distance(c);
+        assert!((1150000.0..1170000.0).contains(&distance));
     }
 }
